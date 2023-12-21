2fd85cb3ed1fb1b2ea67c751f152ac557c26a4bf
Lika Serobiani <likaserobiani@Likas-MacBook-Air.local>
2023-12-21 17:21:10 +0400

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5D37F3",
+        black: "#1A1A1F",
+        success: "#14D81C",
+        error: "#EA1919",
+      },
+    },
   },
   plugins: [],
 };
