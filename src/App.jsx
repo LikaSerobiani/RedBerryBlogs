c5b400c30834621b8720e80b1aa56d2a99925838
Lika Serobiani <likaserobiani@Likas-MacBook-Air.local>
2023-12-21 18:36:13 +0400

    import component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./layout/Header";
-
+import HomePage from "./pages/HomePage";
 export default function App() {
   return (
     <div>
       <Header />
+      <HomePage />
     </div>
   );
 }
