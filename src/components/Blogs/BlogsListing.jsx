0fb7be74073158d79503613c44cd3765bc545b7f
Lika Serobiani <likaserobiani@Likas-MacBook-Air.local>
2023-12-22 13:44:38 +0400

    Add BlogsListing component with data fetching

---
+++ src/components/Blogs/BlogsListing.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import BlogCard from "./BlogCard";
+
+import { get } from "../../api/api";
+import { useState } from "react";
+
+export default function BlogsListing() {
+  const [blogs, setBlogs] = useState([]);
+  const token = import.meta.env.VITE_API_TOKEN;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await get("/blogs", {}, token);
+        setBlogs(result.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="flex flex-wrap justify-center">
+        {blogs.map((blog) => (
+          <div key={blog.id}>
+            <BlogCard
+              image={blog.image}
+              author={blog.author}
+              publish_date={blog.publish_date}
+              title={blog.title}
+              description={blog.description}
+              categories={blog.categories}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
