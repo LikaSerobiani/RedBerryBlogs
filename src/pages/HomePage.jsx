4c3b422b98dde1d03c39d8b32fc460bcbae0a62f
Lika Serobiani <likaserobiani@Likas-MacBook-Air.local>
2023-12-22 13:50:19 +0400

    add BlogsListing component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
-import React from "react";
 import Blog from "../assets/images/Blog.png";
-export default function HomePage() {
+
+import BlogsListing from "../components/Blogs/BlogsListing";
+
+// HomePage.jsx
+export default function HomePage({ blogs }) {
   return (
     <div className="container">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mt-[64px] mb-[64px]">
         <span className="text-[64px] leading-[72px] font-bold not-italic">
           ბლოგი
         </span>
         <img src={Blog} />
       </div>
+      <BlogsListing blogs={blogs} />
     </div>
   );
 }
