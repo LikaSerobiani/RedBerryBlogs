3169ed60aabd3c8b3db36176524c187b3a800e62
Lika Serobiani <likaserobiani@Likas-MacBook-Air.local>
2023-12-23 05:19:54 +0400

    add router link

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="border-b border-solid border-gray-300 bg-white">
       <div className="container h-[80px] flex justify-between items-center shrink-0">
         <div>
-          <img src={Logo} />
+          <Link to="/">
+            <img src={Logo} />
+          </Link>
         </div>
         <div>
           <button className="bg-primary py-[10px] px-[20px] rounded-[8px] text-white font-medium text-sm cursor-pointer">
