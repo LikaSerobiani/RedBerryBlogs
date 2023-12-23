c2427f4f085df0bf04093f5d7f538dc8a0f3bb2b
Lika Serobiani <likaserobiani@Likas-MacBook-Air.local>
2023-12-23 05:12:50 +0400

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./layout/Header";
 import HomePage from "./pages/HomePage";
+import SingleBlogPage from "./pages/SingleBlogPage";
+
 export default function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <HomePage />
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blogs/:id" element={<SingleBlogPage />} />
+      </Routes>
+    </Router>
   );
 }
